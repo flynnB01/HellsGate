7c1b1d18ac688779f548b37150de2d897bd27234
raph426 <132526016+raph426@users.noreply.github.com>
2024-08-29 01:03:43 +1200

    Implemented Main Menu and Options
    Basic menu works. Need to add extra options to options menu and make it actually work. Need to add extra items to menu like continue or load game

---
+++ Hells Gate/Assets/MenuScripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  public void PlayGame()
+    {
+        Debug.Log("Game Start");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Exited Game");
+        Application.Quit();
+    }
+}
