e805073ae8cc023586fe29c66052d9ed8c84ba31
KyleBaltazar04 <132859672+KyleBaltazar04@users.noreply.github.com>
2024-09-19 00:57:15 +1200

    More changes to HealthPacks
    Idk if this works rn

--- Hells Gate/Assets/PlayerScripts/HealthPackSpawner.cs
+++ Hells Gate/Assets/PlayerScripts/HealthPackSpawner.cs
@@ -4,58 +4,23 @@ using UnityEngine;
 
 public class HealthPackSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
     public bool active = true;
-    public HealthPack healthPack;
     public float packCooldown = 5f;
-    public float packCurrentTime;
     public GameObject objectToSpawn;
 
-    public character player;
-    public int healing;
-    void Start()
+    public void SpawnPack()
     {
-        active = true;
-        packCurrentTime = packCooldown;
-        
-    }
-
-    // Update is called once per frame
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // if enemy detects player collision 
-        if (collision.gameObject.CompareTag("Player") && active == true)
+        if (active)
         {
-            Debug.Log("Contact with Health Pack");
-            healthPack.healPlayer();
-            active = false;
+            Instantiate(objectToSpawn, transform.position, transform.rotation);
+            active = false; // Set spawner to inactive after spawning
             StartCoroutine(PackTimer());
         }
     }
 
     private IEnumerator PackTimer()
     {
-        // Wait for the cooldown duration
         yield return new WaitForSeconds(packCooldown);
-
-        // After the wait, spawn a new health pack
-        SpawnPack();
         active = true; // Set the spawner back to active
-
-    }
-    private void SpawnPack()
-    {
-        if(active == true)
-        {
-            Instantiate(objectToSpawn, transform.position, transform.rotation);
-        }
-        
-    }
-
-    public void healPlayer()
-    {
-        Debug.Log("Player Healed");
-        player.ReceiveHealth(healing);
-        Destroy(gameObject);
     }
 }
