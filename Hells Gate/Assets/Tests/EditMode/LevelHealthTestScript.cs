90f51680468e6184c622a920063ba4a92c2f8215
Flynn Butler <flyn.butler@gmail.com>
2024-10-14 22:54:06 +1300

    added unit tests framework and a unit test

---
+++ Hells Gate/Assets/Tests/EditMode/LevelHealthTestScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Character
+{
+    public int maxHp;
+
+    public Character()
+    {
+        // Initializing the character's max HP
+        maxHp = 100;
+    }
+
+    public void incremenntHp()
+    {
+        maxHp += 10;
+    }
+}
+
+public class levelHealthTestScript
+{
+    [Test]
+    public void testLeveHealth()
+    {
+        // Create an instance of the character
+        Character character = new Character();
+
+        // Ensure the initial maxHp is as expected
+        Assert.AreEqual(100, character.maxHp);
+
+        // Simulate a level-up
+        character.incremenntHp();
+
+        // Check if the maxHp increased correctly after leveling up
+        Assert.AreEqual(110, character.maxHp);
+    }
+}
+
