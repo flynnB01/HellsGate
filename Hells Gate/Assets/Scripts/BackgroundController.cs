d5541ed26337b0b9155e55c47305b1ed59017c12
raph426 <132526016+raph426@users.noreply.github.com>
2024-10-21 22:46:11 +1300

    background y-axis movement
    bg now moves on the y-axis, level 3 boss can turn around aswl

--- Hells Gate/Assets/Scripts/BackgroundController.cs
+++ Hells Gate/Assets/Scripts/BackgroundController.cs
@@ -6,32 +6,35 @@ using UnityEngine.UIElements;
 public class BackgroundController : MonoBehaviour
 {
 
-    private float startPos, length;
-    public GameObject camera;
+    private float startPos_x, startPos_y, length;
+    public new GameObject camera;
     public float parallax; // speed bg moves with camera
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position.x;
+        startPos_x = transform.position.x;
+        startPos_y = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void Update()
     {
         // 0 = moving with camera, 1 = not moving, 0.5 = in between
-        float distance = camera.transform.position.x * parallax;
+        float distance_x = camera.transform.position.x * parallax;
+        float distance_y = camera.transform.position.y * parallax;
         float movement = camera.transform.position.x * (1 - parallax);
 
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
 
-        if (movement > startPos + length) // if bg has moved to the end of its own length, change position to give illusion of infinite scrolling
+        transform.position = new Vector3(startPos_x + distance_x, startPos_y + distance_y, transform.position.z);
+
+        if (movement > startPos_x + length) // if bg has moved to the end of its own length, change position to give illusion of infinite scrolling
         {
-            startPos += length;
+            startPos_x += length;
         }
-        else if (movement < startPos - length)
+        else if (movement < startPos_x - length)
         {
-            startPos -= length;
+            startPos_x -= length;
         }
     }
 }
