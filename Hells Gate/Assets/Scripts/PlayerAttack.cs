7bd76b6584ae5aa601dfbb70e6e1b7acfc3c4cc2
YJason <70372800+platinumwolaopo@users.noreply.github.com>
2024-10-22 00:17:43 +1300

    map function made. able to change weapon(not finish yet, need frames about attack, only change weapon stats). gun's bullet not finish yet

--- Hells Gate/Assets/Scripts/PlayerAttack.cs
+++ Hells Gate/Assets/Scripts/PlayerAttack.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private BoxCollider2D box;
-   
+    private int damage;
 
     private void Start()
     {
         box = GetComponent<BoxCollider2D>();
     }
-
+    public void SetWeaponDamage(int weaponDamage)
+    {
+        damage = weaponDamage;
+    }
     //when play attack, turn attackbox on
     public void AtkStar()
     {
@@ -33,13 +36,13 @@ public class PlayerAttack : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            collision.gameObject.GetComponent<enemy>().takeDmg(10);
+            collision.gameObject.GetComponent<enemy>().takeDmg(damage);
 
         }
         if (collision.gameObject.CompareTag("Boss"))
         {
             Boss boss = collision.gameObject.GetComponent<Boss>();  
-            boss.takeDmg(10); // TODO - change dmg value to correspond with player stats
+            boss.takeDmg(damage); // TODO - change dmg value to correspond with player stats
         }
         
     }
--- Hells Gate/Assets/Scripts/Weapon/WeaponController.cs
+++ Hells Gate/Assets/Scripts/Weapon/WeaponController.cs
@@ -1,37 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public class Weapon
+{
+    public int id;
+    public string name;
+    public int damage;
+    public int style;
+    public GameObject bullets;
+}
 
 public class WeaponController : MonoBehaviour
 {
     public Animator ani;
     public GameObject atkPos;//attack start spot
     bool isAtk;//check able to attack
-
+    public Weapon[] weapons;
+    Weapon nowWeapon;
     // Start is called before the first frame update
     void Start()
     {
         ani = GetComponent<Animator>();
-        ChageWeapon(1);//weapon change
+        //ChageWeapon(1);//weapon change
       
         isAtk = true;
+        InitWeapon();
     }
 
-    public void ChageWeapon(float id)
+    public void InitWeapon()
     {
+        nowWeapon = weapons[0];
+        atkPos.GetComponent<PlayerAttack>().SetWeaponDamage(weapons[0].damage);
+        ani.SetFloat("weapon 0", 0);
+    }
+
+    public void ChageWeapon(int id) 
+    {
+        nowWeapon = weapons[id - 1];
         ani.SetFloat("weapon 0", id);
+        atkPos.GetComponent<PlayerAttack>().SetWeaponDamage(weapons[id-1].damage);
+       //weapon change
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1") && isAtk) // check if attack
         {
-            Debug.Log("Attack");
-            ani.Play("P_sword");
-            Attack(); // invoke attack
+            if (nowWeapon.style == 0)
+            {
+                Debug.Log("Attack");
+                ani.Play("P_sword");
+                Attack(); // invoke attack
+            }
+            else if (nowWeapon.style==1)
+            {
+                
+                rangerAttack();
+            }
+           
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChageWeapon(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChageWeapon(3);
         }
     }
-
+    public void rangerAttack()
+        {
+        isAtk = false; 
+        GameObject bullet = GameObject.Instantiate(nowWeapon.bullets);
+        bullet.transform.position = atkPos.transform.position;
+        bullet.transform.localScale = transform.localScale;
+        StartCoroutine(WaitShoot());
+        }
    
 
     void Attack()//attack 
@@ -54,6 +99,12 @@ public class WeaponController : MonoBehaviour
     }
 
 
+    IEnumerator WaitShoot()//time wait for next shoot
+    {
+        yield return new WaitForSeconds(1f);
+        atkPos.SetActive(false);
+        isAtk = true;
+    }
 
 
 
