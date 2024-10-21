c7b555c4d2ed4a281159c869fe4834e9c01a8ba2
KyleBaltazar04 <132859672+KyleBaltazar04@users.noreply.github.com>
2024-10-22 00:49:50 +1300

    Added Unit Test and level 2 layout

--- Hells Gate/Assets/Tests/EditMode/LevelHealthTestScript.cs
+++ Hells Gate/Assets/Tests/EditMode/LevelHealthTestScript.cs
@@ -1,41 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
-public class Character
-{
-    public int maxHp;
-
-    public Character()
-    {
-        // Initializing the character's max HP
-        maxHp = 100;
-    }
-
-    public void incremenntHp()
-    {
-        maxHp += 10;
-    }
-}
-
-public class levelHealthTestScript
+public class LevelHealthTestScript
 {
     [Test]
-    public void testLeveHealth()
+    public void Test_NextLevelLoadScript()
     {
-        // Create an instance of the character
-        Character character = new Character();
+        GameManager gameManager = new();
+        
 
-        // Ensure the initial maxHp is as expected
-        Assert.AreEqual(100, character.maxHp);
+        gameManager.nextLevel = 2;
 
-        // Simulate a level-up
-        character.incremenntHp();
+        gameManager.LoadNextLevel();
 
-        // Check if the maxHp increased correctly after leveling up
-        Assert.AreEqual(110, character.maxHp);
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        int expected = 2;
+
+        Assert.AreEqual(expected, currentScene);
     }
 }
-
